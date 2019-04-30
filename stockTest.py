8f9094525d81d71254948c53cd2907c480525371
new sky <39216692+flysunys@users.noreply.github.com>
2019-04-30 15:23:55 +0800

    Update stockTest.py
    add a figure use method

--- stockTest.py
+++ stockTest.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plf
 start=datetime.date(2012,1,1)
 end=datetime.date.today()
 stock=web.DataReader('601009.SS','yahoo',start,end)
+stock.plot()
+plf.show()
 #stock.to_csv(r'D:\workfiles\stock\data\njyh_601009.csv',columns=stock.columns,index=True)
 print(stock['Close'])
 fig=plf.figure()
