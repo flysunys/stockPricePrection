0eb434b5ec4f5e0511b5fe61af4e4ec8c010b70e
flysun <flysun_wfy@163.com>
2019-07-23 00:01:05 +0800

    [wfy]学会了post和get的区别，以及如何在python中使用

--- stockWeb/index.py
+++ stockWeb/index.py
@@ -7,16 +7,26 @@ app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
 	if request.method == "GET":
+		print(request.args.get('start_time'))
 		return render_template("index_bak.html")
 	if request.method == "POST":
-		print(request.headers)
-		request_info = request.values.to_dict();
-		print(request_info.get("start_time"))
-		if request.data == None:
-			print("返回值为空")
-		print(request.json)
-		print(request.data)
-		return redirect("/")
+		#print(request.form.get("start_time"))
+		#request_info = request.values.to_dict();
+		#print(request_info.get("start_time"))
+		#if request.data == None:
+		#	print("返回值为空")
+		#else:
+		#	print("返回值不为空")
+		#print(request.json)
+		#print(request.data)
+		#print(request.form)
+		#return redirect("/")
+		starttime=request.form['start_time']
+		endtime=request.form['end_time']
+		stockcode=request.form['stock_code']
+		print(starttime,endtime,stockcode)
+		
+		return '<h1>你好<h1>'
 	return "请重新编写路由"
 if __name__ == '__main__':
 	app.run(debug=True)
