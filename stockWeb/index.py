f1418eacc21290b898e92b36d7b79ee0635080c4
flysun <flysun_wfy@163.com>
2019-07-28 23:53:43 +0800

    [wfy]add learning

--- stockWeb/index.py
+++ stockWeb/index.py
@@ -9,11 +9,36 @@ pth.sys.path.append(p)
 from dbstock import DBStock
 app=Flask(__name__)
 
+def insertalltotable(stock,stock_code):
+	for x1,x2,x3,x4,x5,x6,x7 in zip(stock.index,stock['High'].values,stock['Low'].values,stock['Open'].values,stock['Close'].values,stock['Volume'].values,stock['Adj Close'].values):
+		stockdate=x1.date()
+		stockcode=stock_code
+		stockhigh=float(round(x2,2))
+		stocklow=float(round(x3,2))
+		stockopen=float(round(x4,2))
+		stockclose=float(round(x5,2))
+		stockvolume=float(round(x6,2))
+		stockadj=float(round(x7,2))
+		#print(type(stockdate))
+		#print(type(stockhigh))
+		#print(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj)
+		DBStock.insert_of_table(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj)
+
+def dateRange(beginDate, endDate):
+    dates = []
+    dt = datetime.datetime.strptime(beginDate, "%Y-%m-%d")
+    date = beginDate[:]
+    while date <= endDate:
+        dates.append(date)
+        dt = dt + datetime.timedelta(1)
+        date = dt.strftime("%Y-%m-%d")
+    return dates
+
 @app.route('/',methods=['GET','POST'])
 def index():
 	if request.method == "GET":
 		print(request.args.get('start_time'))
-		table_name="StockDataBackup"
+		table_name="stockdatabackup"
 		if DBStock.exit_of_table(table_name):
 			print("存在此表")
 		else:
@@ -35,9 +60,26 @@ def index():
 		starttime=request.form['start_time']
 		endtime=request.form['end_time']
 		stockcode=request.form['stock_code']
+		#print(type(starttime))
+		#print(type(stockcode))
+		#date_starttime = datetime.datetime.strptime(starttime,'%Y-%m-%d').date()
+		#date_endtime = datetime.datetime.strptime(endtime,'%Y-%m-%d').date()
+		#stock_data_ss=web.DataReader(stockcode + '.SS','yahoo',date_starttime,date_endtime)
+		#insertalltotable(stock_data_ss,stockcode)
+		list_query_data=[]
+		for date_day in dateRange(starttime,endtime):
+			print(date_day)
+			result_query=DBStock.query_of_table(date_day,stockcode)
+			if DBStock.query_of_table(date_day,stockcode):
+				print("exit %s %s" % (date_day,stockcode))
+				list_query_data.append(result_query[0])
+			else:
+				print("not exit %s %s" % (date_day,stockcode))
+			
+				
 		print(starttime,endtime,stockcode)
 		
-		return '<h1>你好<h1>'
+		return render_template("index_bak.html",list_query_data=list_query_data)
 	return "请重新编写路由"
 if __name__ == '__main__':
 	app.run(debug=True)
