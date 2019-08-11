d273cdae5fe067473dae43e53c8d84cea0c0c0b2
flysun <flysun_wfy@163.com>
2019-08-11 23:25:31 +0800

    [wfy]add label

--- stockWeb/DButils/netdatastock.py
+++ stockWeb/DButils/netdatastock.py
@@ -1,6 +1,7 @@
 import pymysql
 import datetime
 import re
+import mysql.connector
 
 class DBDataNet:
 	def establishTable():
@@ -58,13 +59,46 @@ class DBDataNet:
 		cursor.close()
 		conn.close()
 		return result
-	def insert_of_table(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj):
+	def query_for_table(param):
+		conn = pymysql.connect(host="localhost",user="root",password="1234",database="test",charset="utf8")
+		cursor=conn.cursor()
+		sql="SELECT "+param+" FROM `NetDataStock`"
+
+		cursor.execute(sql)
+		conn.commit()
+		result = cursor.fetchall()
+		cursor.close()
+		conn.close()
+		return result
+	def update_for_table(param,value1,value2,value3):
+		conn = pymysql.connect(host="localhost",user="root",passwd="1234",database="test",charset="utf8")
+		cursor=conn.cursor()
+		print(type(value2))
+		print(value2.strftime("%Y-%m-%d"))
+		#print(type(datetime.datetime.strptime(value2.strftime("%Y-%m-%d"),'%Y-%m-%d')))
+		#value2="DATE_FORMAT("+value2.strftime("%Y-%m-%d")+",'%Y-%m-%d')"
+		#sql="UPDATE `NetDataStock` SET "+param+" = "+value1+" WHERE `StockDate` = "+value2+" AND `StockCode` = "+value3
+		#sql="UPDATE `NetDataStock` SET %s = %d WHERE `StockDate` = %s AND `StockCode` = %s" % (param,value1,value2,value3)
+		#sql="UPDATE `NetDataStock` SET %s = %s WHERE `StockDate` = %s AND `StockCode` = %s"
+		sql="UPDATE `NetDataStock` SET '{}' = {} WHERE `StockDate` = {} AND `StockCode` = {}"
+		#print(sql)
+		sql1=sql.format(param,value1,value2,value3)
+		print(sql1)
+		cursor.execute(sql1)
+		#cursor.execute(sql,(param,value1,value2.strftime("%Y-%m-%d"),value3))
+		#cursor.execute(sql)
+		conn.commit()
+		result = cursor.fetchall()
+		cursor.close()
+		conn.close()
+		return result
+	def insert_of_table(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj,label):
 		conn = pymysql.connect(host="localhost",user="root",password="1234",database="test",charset="utf8")
 		cursor=conn.cursor()
 
-		sql="Insert INTO `NetDataStock` (`StockDate`,`StockCode`,`StockHigh`,`StockLow`,`StockOpen`,`StockClose`,`StockVolume`,`StockAdj`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+		sql="Insert INTO `NetDataStock` (`StockDate`,`StockCode`,`StockHigh`,`StockLow`,`StockOpen`,`StockClose`,`StockVolume`,`StockAdj`,`Label`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-		cursor.execute(sql,(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj))
+		cursor.execute(sql,(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj,label))
 		
 		conn.commit()
 		result = cursor.fetchone()
--- stockWeb/stockDataNet.py
+++ stockWeb/stockDataNet.py
@@ -14,6 +14,35 @@ import sys
 sys.path.append(r'G:\gitdir\gitprojects\stockPricePrection\stockWeb\DButils')
 from netdatastock import DBDataNet
 
+def insertalltotable(stock,stock_code):
+	#print(stock['close'].values[1:])
+	labelnum_a=stock['close'].values
+	labelnum_b=labelnum_a[:-1]
+	labelnum_c=labelnum_b.tolist()
+	labelnum_c.insert(0,3.3)
+	#print(labelnum_c)
+	#print(type(labelnum_b.tolist()))
+	#labelnum_c=[0,labelnum_b]
+	for x1,x2,x3,x4,x5,x6,x7,x8 in zip(stock.index,stock['high'].values,stock['low'].values,stock['open'].values,stock['close'].values,stock['volume'].values,stock['p_change'].values,labelnum_c):
+		if DBDataNet.query_of_table(x1,stock_code):
+			print("exit each_day in stockDB")
+		else:
+			print("not exit each_day in stockDB")
+			stockdate=x1
+			stockcode=stock_code
+			stockhigh=float(round(x2,2))
+			stocklow=float(round(x3,2))
+			stockopen=float(round(x4,2))
+			stockclose=float(round(x5,2))
+			stockvolume=float(round(x6,2))
+			stockadj=float(round(x7,2))
+			label=x8
+			print(type(x8))
+			#print(type(stockdate))
+			#print(type(stockhigh))
+			#print(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj)
+			DBDataNet.insert_of_table(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj,label)
+
 table_name="netdatastock"
 if DBDataNet.exit_of_table(table_name):
 	print("存在此表")
@@ -23,33 +52,28 @@ else:
 
 #print([[1,1],[2,2],[3,3]])
 #df = ts.shibor_ma_data()
-stockcode='603993'
-start_str='2019-01-01'
-end_str='2019-01-12'
+stockcode='601668'
+start_str='2017-06-08'
+end_str='2019-08-09'
 starttime=datetime.datetime.strptime(start_str,'%Y-%m-%d')
 endtime=datetime.datetime.strptime(end_str,'%Y-%m-%d')
-df = ts.get_hist_data('603993',start='2019-01-01',end='2019-01-12')
+df = ts.get_hist_data(stockcode,start=start_str,end=end_str)
 date_list = []
 date_list.append(starttime)
-#print(df)
+#temp=df.index
+#print(df.index[:-1])
 while starttime < endtime:
 	starttime+=datetime.timedelta(days=+1)
 	date_list.append(starttime)
-print(date_list)
-for each_day in date_list:
-	if DBDataNet.query_of_table(each_day,stockcode):
-		print("exit each_day in stockDB")
-	else:
-		print("not exit each_day in stockDB")
-		stockdate=each_day
-		stockcode=stockcode
-		stockhigh=df['high'].values[0]
-		stocklow=df['low'].values[0]
-		stockopen=df['open'].values[0]
-		stockclose=df['close'].values[0]
-		stockvolume=df['volume'].values[0]
-		stockadj=df['p_change'].values[0]
-		DBDataNet.insert_of_table(stockdate,stockcode,stockhigh,stocklow,stockopen,stockclose,stockvolume,stockadj)
+#print(date_list)
+insertalltotable(df,stockcode)
+close_price_list=DBDataNet.query_for_table("StockClose")
+stock_date_list=DBDataNet.query_for_table("StockDate")
+#print(close_price_list[:-1])
+#print(type(1.1))
+#for each_price, each_date in zip(close_price_list[1:],stock_date_list[:-1]):
+#	print(type(each_price[0]),type(each_date[0]))
+	#DBDataNet.update_for_table("Label",each_price[0],each_date[0],stockcode)
 #print(type(df))
 #result_shibors=df.values
 #for each_day in df:
