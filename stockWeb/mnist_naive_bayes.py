609f4ac207dc2602a060eecb0624a5ebd2d76046
flysun <flysun_wfy@163.com>
2019-09-06 23:38:43 +0800

    [wfy]add discrete data and start to calculate bayes

--- stockWeb/mnist_naive_bayes.py
+++ stockWeb/mnist_naive_bayes.py
@@ -24,30 +24,8 @@ class NaiveBayes():
 		for i in range(x_array.shape[0]):
 			t[i,:]=x_array[i]
 		return t
-	def discrete_data(self,X,level=5):
-		"""
-		离散化数据
-		设置数据的模糊边界
-		"""
-		#set paramter
-		m,n=X.shape
-		for i in range(n):
-			unit_level=int(max(X[:,i])//level)
-			unit_level_array=
-		#p=2,so this distance is ER diatance
-		distance_test=np.sum(test_r,axis=1)
-		index_test=np.argsort(distance_test)#从小到大排序的
-		#print(index_test)
-		#print(index_test[0:7])
-		#print(Y[index_test[0:7],0])
-		test_k_np=Y[index_test[0:7],0].tolist()
-		list_count=[]
-		for i in range(self.k):
-			list_count.append(test_k_np.count(i))
-		return list_count.index(max(list_count))
 		
-			
-	def knn_kd_tree_balance(self,x_data,y_data,X,Y,iters,learnning_rate):
+	def Naive_Bayes_Calculate(self,X,Y):
 		#set paramter
 		#alpha_r=0*np.random.random((X.shape[0],1)) #初始设置为0也可以
 		alpha_r=3.5*np.abs(np.random.random((X.shape[0],1)))
@@ -94,20 +72,30 @@ if __name__=='__main__':
 	#df_virginica=df_virginica.drop([0,1],axis=1)
 	########合并df
 	df_data_one=pd.concat([df_setosa,df_versicolor,df_virginica], axis=0)
+	
 	df_data_one_replace=df_data_one.replace('Iris-setosa',0)
 	df_data_two=df_data_one_replace.replace('Iris-versicolor',1)
 	df_data_three=df_data_two.replace('Iris-virginica',2)
 	########covert data to np.array
 	df_data_three = pd.DataFrame(df_data_three)
 	df_data_three=df_data_three.astype('float')
-	np_data_three=df_data_three.values
+	#print(pd.cut(df_data_three[0],3,labels=range(3)))
+	#print(pd.cut(df_data_three[0],3,labels=[5,6,7]))
+	#####discrete data
+	df_data_three_dis=df_data_three
+	df_data_three_dis[0]=pd.cut(df_data_three[0],3,labels=[5,6,7])
+	df_data_three_dis[1]=pd.cut(df_data_three[1],3,labels=[2,3,4])
+	df_data_three_dis[2]=pd.cut(df_data_three[2],6,labels=[1,2,3,4,5,6])
+	df_data_three_dis[3]=pd.cut(df_data_three[3],4,labels=[0,1,2,3])
+	#print(df_data_three_dis)
+	np_data_three=df_data_three_dis.values
+	#print(np_data_three)
 	#np_data_two=np_data_two.astype('float')
-	########instance to object 
-	instance_one=knn_data()
+	########instance to object
+	instance_one=NaiveBayes()
+	instance_one.Naive_Bayes_Calculate(np_data_three[:,0:4],np_data_three[:,4])
 	########set paramters
 	#使用测试数据测试普通的mnist
-	
-	test_kind=instance_one.knn_normal(np_data_three[:,0:4],instance_one.array_oneTotwo(np_data_three[:,4]),test_data)
 	
 	
 	
