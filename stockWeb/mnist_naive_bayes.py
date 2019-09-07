053f7ff2767dbcc78226314409a2609c22c290d4
new sky <39216692+flysunys@users.noreply.github.com>
2019-09-07 15:37:47 +0800

    calculate naive bayes probility

--- stockWeb/mnist_naive_bayes.py
+++ stockWeb/mnist_naive_bayes.py
@@ -27,34 +27,22 @@ class NaiveBayes():
 		
 	def Naive_Bayes_Calculate(self,X,Y):
 		#set paramter
-		#alpha_r=0*np.random.random((X.shape[0],1)) #初始设置为0也可以
-		alpha_r=3.5*np.abs(np.random.random((X.shape[0],1)))
-		b=0
-		#ones_data=np.zeros(X.shape[0])+1
-		#X=np.insert(X,0,ones_data,axis=1)  #这里w和b是分开的，对初始数据不需要添加1
-		gram_marix=np.dot(X,X.T)
-		count_iter=0
-		for i in range(iters):
-			f_data=np.sum(X*alpha_r*Y,axis=0)
-			f_data_two=self.array_oneTotwo(f_data)
-			Y_estimates=np.dot(X,f_data_two)+b
-			#print(Y_estimates*Y)
-			idx=np.where(Y_estimates*Y<=0)
-			if len(idx[0])==0:
-				print("全部分类正确")
-				break
-			count_iter+=1
-			cost_per=-np.sum(Y_estimates[idx[0],:]*Y[idx[0],:])
-			alpha_r[idx[0],:]=alpha_r[idx[0],:]+learnning_rate
-			b=b+learnning_rate*np.sum(Y[idx[0],:],axis=0)
-		print("迭代总次数是：%s" % (count_iter))
-		w_data=f_data
-		b_data=b
-		x_test=np.linspace(0,20,num=20)
-		plf.scatter(x_data,y_data)
-		plf.plot(x_test,(-w_data[0]*x_test-b_data)/w_data[1])
-		plf.show()
-	
+		m,n=np.shape(X)
+		Y_unique=np.unique(Y)
+		Y_probility=[]
+		for i in Y_unique:
+    			#print(np.where(b==i))
+    			num=np.shape(np.where(Y==i))[1]
+    			print(num)
+    			Y_probility.append(1.0*num/np.shape(Y)[0])
+		print(Y_probility)
+		for j in Y_unique:
+			idx=np.where(Y==i)
+			for k in range(n):
+				x_k_unique=np.unique(X[idx,k]
+		
+		
+			
 	
 
 if __name__=='__main__':
