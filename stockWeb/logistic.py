187a86ddf31a721b54e45ec55b1a058279380580
flysun <flysun_wfy@163.com>
2019-09-20 15:03:32 +0800

    [wfy]impl logistic for 2 types

--- stockWeb/logistic.py
+++ stockWeb/logistic.py
@@ -17,12 +17,13 @@ from sklearn import svm
 from sklearn import linear_model
 import matplotlib.pyplot as plf
 import requests
+import logging
 #from mnist_db import mnist_data  #对于同一目录下调用可以如此
 from DButils.mnist_database import mnist_data  #调用子目录下的python文件，需要加目录名称
 
 
 
-class perceptron_data():
+class logistic_data():
 	def __init__(self):
 		self.c=0.01
 		self.alpha=0.2
@@ -37,7 +38,9 @@ class perceptron_data():
 	def figure_scatter(self,x_data,y_data):
 		plf.scatter(x_data,y_data)
 		plf.show()
-	def perceptron_fit_normal(self,x_data,y_data,X,Y,iters,alpha):
+	def sigmod(self,x):
+		return 1.0/(1.0+np.exp(-x))
+	def logistic_fit_normal(self,x_data,y_data,X,Y,iters,alpha):
 		#set paramter
 		theta=6*np.random.random((X.shape[1]+1,1))
 		ones_data=np.zeros(X.shape[0])+1
@@ -49,17 +52,17 @@ class perceptron_data():
 		for i in range(iters):
 			Y_estimates=np.dot(X,theta)
 			#print(Y_estimates*Y)
-			idx=np.where(Y_estimates*Y<0)
-			if len(idx[0])==0:
+			Y_S=self.sigmod(Y_estimates)
+			diff=Y_S-Y
+			tempw=theta-alpha*np.dot(X.T,diff)
+			print(tempw)
+			print(np.sqrt(sum((tempw-theta)**2)))
+			if np.sqrt(sum((tempw-theta)**2))<0.002:
 				print("全部分类正确")
 				break
-			#print(idx)
-			#print(Y_estimates[idx],Y[idx])
-			#print(X[idx[0],:],Y[idx[0],:])
+			else:
+				theta=tempw
 			count_iter+=1
-			cost_per=-np.sum(Y_estimates[idx[0],:]*Y[idx[0],:])
-			grand_w=-np.dot(X[idx[0],:].T,Y[idx[0],:])
-			theta=theta-alpha*grand_w
 		print("迭代总次数是：%s" % (count_iter))
 		coef_data=theta
 		x_test=np.linspace(0,20,num=20)
@@ -111,7 +114,7 @@ if __name__=='__main__':
 	df_versicolor=df_versicolor.drop([0,1],axis=1)
 	########合并df
 	df_data_one=pd.concat([df_setosa, df_versicolor], axis=0)
-	df_data_one_replace=df_data_one.replace('Iris-setosa',-1)
+	df_data_one_replace=df_data_one.replace('Iris-setosa',0)
 	df_data_two=df_data_one_replace.replace('Iris-versicolor',1)
 	########covert data to np.array
 	df_data_two = pd.DataFrame(df_data_two)
@@ -119,16 +122,18 @@ if __name__=='__main__':
 	np_data_two=df_data_two.values
 	#np_data_two=np_data_two.astype('float')
 	########instance to object 
-	instance_one=perceptron_data()
+	instance_one=logistic_data()
 	########set paramters
-	iters=500
-	alpha=0.1
+	iters=50
+	alpha=0.08
 	learningrate=1
 	#如果theta初始值的斜率是正，收敛出的也是正，如果是负，收敛出的也是负，因为这个数据的可分超平面太多了，明显的线性可分
-	#普通感知机进行训练
-	#instance_one.perceptron_fit_normal(np_data_two[:,0],np_data_two[:,1],np_data_two[:,0:2],instance_one.array_oneTotwo(np_data_two[:,2]),iters,alpha)
-	#使用对偶形式的感知机进行训练
-	instance_one.perceptron_fit_pair(np_data_two[:,0],np_data_two[:,1],np_data_two[:,0:2],instance_one.array_oneTotwo(np_data_two[:,2]),iters,learningrate)
+	#logistic 2 分类
+	logger=logging.getLogger()
+	logger.setLevel(logging.INFO)
+	instance_one.logistic_fit_normal(np_data_two[:,0],np_data_two[:,1],np_data_two[:,0:2],instance_one.array_oneTotwo(np_data_two[:,2]),iters,alpha)
+	#logistic 多 分类
+	#instance_one.perceptron_fit_pair(np_data_two[:,0],np_data_two[:,1],np_data_two[:,0:2],instance_one.array_oneTotwo(np_data_two[:,2]),iters,learningrate)
 	#instance_one.figure_scatter(np_data_two[:,0],np_data_two[:,1])
 	########
 	
