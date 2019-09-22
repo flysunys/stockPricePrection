c6720ad42d9c6e18e0fa34211f0b82e053905d43
flysun <flysun_wfy@163.com>
2019-09-22 23:45:19 +0800

    [wfy]imply max entropy model

--- stockWeb/max_entropy_model.py
+++ stockWeb/max_entropy_model.py
@@ -1,11 +1,16 @@
 #coding:utf8
 
-#author:flysun
-#date:2019-09-20
+#@author:flysun
+#@date:2019-09-20
+#@last modified by:flysun
+#@last modified time:2019-09-22
+
 """
 description:
 
-最大熵模型用于多分类情况，以二分类为例，逻辑斯蒂回归模型的损失函数
+最大熵原理是用于选择一个最优的概率模型，熵最大的模型就是最优概率模型，属于模型优化的范围
+
+将最大熵原理应用到分类得到了最大熵模型，也就是最大熵模型是一个分类模型
 
 对于多分类的情况，如果各类是明显分开的，且各类中心不在一条直线上，使用分别训练各类的线性权值的方式是可行的；
 
@@ -14,6 +19,11 @@ description:
 """
 
 import pandas as pd
+import time
+import math
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import numpy as np
 import scipy.optimize as opt
 #from scipy.optimize import fmin_slsqp
@@ -22,19 +32,32 @@ from sklearn import linear_model
 import matplotlib.pyplot as plf
 import requests
 import logging
+from collections import defaultdict 
 #from mnist_db import mnist_data  #对于同一目录下调用可以如此
 from DButils.mnist_database import mnist_data  #调用子目录下的python文件，需要加目录名称
 
 
 
 class max_entropy_model_data():
-	def __init__(self):
-		self.c=0.01
-		self.alpha=0.2
+	def init_params(self,X,Y):
+		self.learningrate=0.2
+		self.X_=X
+		self.Y_=set()
+		self.cal_Pxy_Px(X,Y)
+		self.N=len(X)  #训练集大小
+		self.n=len(self.Pxy)    #
+		self.M=10000.0
+		self.build_dict()
+		self.cal_EPxy()
 	def array_oneTotwo(self,x_array):
 		t=np.empty((x_array.shape[0],1))
 		for i in range(x_array.shape[0]):
 			t[i,:]=x_array[i]
+		return t
+	def array_twoToone(self,x_array):
+		t=np.zeros(x_array.shape[0])
+		for i in range(x_array.shape[0]):
+			t[i]=x_array[i][0]
 		return t
 	def figure_plot(self,data):
 		plf.plot(data)
@@ -42,116 +65,102 @@ class max_entropy_model_data():
 	def figure_scatter(self,x_data,y_data):
 		plf.scatter(x_data,y_data)
 		plf.show()
-	def sigmod(self,x):
-		return 1.0/(1.0+np.exp(-x))
-	def maxentropy_fit_normal(self,x_data,y_data,X,Y,iters,alpha):
-		#set paramter
-		theta=6*np.random.random((X.shape[1]+1,1))
-		ones_data=np.zeros(X.shape[0])+1
-		X=np.insert(X,0,ones_data,axis=1)
-		#print(X)
-		#print(type(X[1,1]))
-		#print(X.shape,Y.shape)
-		count_iter=0
-		for i in range(iters):
-			Y_estimates=np.dot(X,theta)
-			#print(Y_estimates*Y)
-			Y_S=self.sigmod(Y_estimates)
-			diff=Y_S-Y
-			tempw=theta-alpha*np.dot(X.T,diff)
-			print(tempw)
-			print(np.sqrt(sum((tempw-theta)**2)))
-			if np.sqrt(sum((tempw-theta)**2))<0.002:
-				print("全部分类正确")
-				break
-			else:
-				theta=tempw
-			count_iter+=1
-		print("迭代总次数是：%s" % (count_iter))
-		coef_data=theta
-		x_test=np.linspace(0,20,num=20)
-		plf.scatter(x_data,y_data)
-		plf.plot(x_test,(-coef_data[1,:]*x_test-coef_data[0,:])/coef_data[2,:])
-		plf.show()
+	def cal_Pxy_Px(self,X,Y):
+		self.Pxy=defaultdict(int)
+		self.Px=defaultdict(int)
+		for i in range(len(X)):
+			x_,y_array = X[i],Y[i]
+			y=y_array[0]
+			self.Y_.add(y)
+			for x in x_:
+				self.Pxy[(x,y)] +=1
+				self.Px[x] +=1
+	def cal_EPxy(self):
+		"""
+		特征函数关于经验分布的期望值
+		"""
+		self.EPxy=defaultdict(int)
+		for id in range(self.n):
+			(x,y)=self.id2xy[id]
+			self.EPxy[id]=float(self.Pxy[(x,y)])/float(self.N)
+			
+	def build_dict(self):
+		self.id2xy={}
+		self.xy2id={}
+		for i,(x,y) in enumerate(self.Pxy):
+			self.id2xy[i]=(x,y)
+			self.xy2id[(x,y)]=i
+			
+	def cal_pxy(self,X,y):
+		result=0.0
+		for x in X:
+			if self.fxy(x,y):
+				id = self.xy2id[(x,y)]
+				print(id)
+				result += self.w[id]
+		return (math.exp(result),y)
 		
-	def logistic_fit_for_three(self,x_data,y_data,X,Y,iters,learnning_rate):
-		#set paramter
-		#alpha_r=0*np.random.random((X.shape[0],1)) #初始设置为0也可以
-		theta_all=[]
-		#theta=6*np.random.random((X.shape[1]+1,1))
-		#print(theta.shape)
-		#print(X.shape)
-		ones_data=np.zeros(X.shape[0])+1
-		X=np.insert(X,0,ones_data,axis=1)
-		#print(X.shape)
-		#print(type(X[1,1]))
-		#print(X.shape,Y.shape)
-		for j in range(3):
-			count_iter=0
-			theta=6*np.random.random((X.shape[1],1))
-			for i in range(iters):
-				Y_estimates=np.dot(X,theta)
-				#print(Y_estimates*Y)
-				Y_S=self.sigmod(Y_estimates)
-				#print(Y_S.shape)
-				#print(Y[:,j].shape)
-				diff=Y_S-self.array_oneTotwo(Y[:,j])
-				#print(diff.shape)
-				tempw=theta-learnning_rate*np.dot(X.T,diff)
-				#print(tempw.shape)
-				#print(np.sqrt(sum((tempw-theta)**2)))
-				theta=tempw
-				count_iter+=1
-			print("迭代总次数是：%s" % (count_iter))
-			theta_all.append(theta)
-		for j in range(3):
-			coef_data=theta_all[j]
-			#print(coef_data)
-			x_test=np.linspace(0,20,num=20)
-			plf.plot(x_test,(-coef_data[1,:]*x_test-coef_data[0,:])/coef_data[2,:])
-		plf.scatter(x_data,y_data)
-		plf.show()
-	
-	def logistic_fit_for_three_nolinear(self,x_data,y_data,X,Y,iters,learnning_rate):
-		#set paramter
-		theta_all=[]
-		ones_data=np.zeros(X.shape[0])+1
-		X=np.insert(X,0,ones_data,axis=1)
-		X=np.insert(X,2,np.square(X[:,1]),axis=1)
-		X=np.insert(X,3,X[:,1]**3,axis=1)
-		#X=np.insert(X,1,1.0/X[:,1],axis=1)
-		#X[:,3]=np.square(X[:,3])
-		#print(X.shape)
-		#print(type(X[1,1]))
-		#print(X.shape,Y.shape)
-		for j in range(3):
-			count_iter=0
-			theta=0.2*np.random.random((X.shape[1],1))
-			for i in range(iters):
-				Y_estimates=np.dot(X,theta)
-				#print(Y_estimates*Y)
-				Y_S=self.sigmod(Y_estimates)
-				#print(Y_S.shape)
-				#print(Y[:,j].shape)
-				diff=Y_S-self.array_oneTotwo(Y[:,j])
-				#print(diff.shape)
-				tempw=theta-learnning_rate*np.dot(X.T,diff)
-				#print(tempw.shape)
-				#print(np.sqrt(sum((tempw-theta)**2)))
-				theta=tempw
-				count_iter+=1
-			print("迭代总次数是：%s" % (count_iter))
-			theta_all.append(theta)
-		for j in range(3):
-			coef_data=theta_all[j]
-			#print(coef_data)
-			x_test=np.linspace(0,8,num=20)
-			#plf.plot(x_test,(-coef_data[1,:]*x_test-coef_data[2,:]*(x_test**2)-coef_data[0,:])/coef_data[3,:])
-			plf.plot(x_test,(-coef_data[1,:]*x_test-coef_data[2,:]*(x_test**2)-coef_data[3,:]*(x_test**3)-coef_data[0,:])/coef_data[4,:])
-			#plf.plot(x_test,(-coef_data[1,:]*(1.0/x_test)-coef_data[2,:]*x_test-coef_data[3,:]*(x_test**2)-coef_data[0,:])/coef_data[4,:])
-		plf.scatter(x_data,y_data)
-		plf.show()
-	
+	def cal_probality(self,X):
+		"""
+		计算最大熵模型对应的条件概率分布
+		"""
+		Pyxs=[(self.cal_pxy(X,y)) for y in self.Y_]
+		Z=sum([prob for prob,y in Pyxs])
+		return [(prob/Z,y) for prob,y in Pyxs]
+		
+	def cal_EPx(self):
+		"""
+		计算特征函数关于模型pyx和模型px的期望值
+		"""
+		self.EPx=[0.0 for i in range(self.n)]
+		
+		for i,X in enumerate(self.X_):
+			Pyxs = self.cal_probality(X)
+			
+			for x in X:
+				for Pyx,y in Pyxs:
+					if self.fxy(x,y):
+						id=self.xy2id[(x,y)]
+						self.EPx[id] += Pyx*(1.0/self.N)
+						
+	def fxy(self,x,y):
+		return (x,y) in self.xy2id
+		
+	def train(self,X,Y):
+		self.init_params(X,Y)
+		self.w=[0.0 for i in range(self.n)]
+		
+		max_iterations = 1000
+		
+		for times in range(max_iterations):
+			print('iterates times %d' % times)
+			sigmas=[]
+			self.cal_EPx()
+			
+			for i in range(self.n):
+				sigma = 1.0 / self.M * math.log(self.EPxy[i]/self.EPx[i])
+				sigmas.append(sigma)
+			self.w=[self.w[i]+sigmas[i] for i in range(self.n)]
+			
+	def predict(self,testset):
+		results=[]
+		for test in testset:
+			result=self.cal_probality(test)
+			results.append(max(result,key=lambda x:x[0])[1])
+		return results
+			
+def rebuild_features(features):
+	"""
+	将原feature的（a0,a1,a2,a3,a4,...）
+    变成 (0_a0,1_a1,2_a2,3_a3,4_a4,...)形式
+	"""
+	new_features=[]
+	for feature in features:
+		new_feature=[]
+		for i,f in enumerate(feature):
+			new_feature.append(str(i)+'_'+str(f))
+		new_features.append(new_feature)
+	return new_features
 	
 
 if __name__=='__main__':
@@ -192,9 +201,6 @@ if __name__=='__main__':
 	df_data_two = pd.DataFrame(df_data_two)
 	df_data_two=df_data_two.astype('float')
 	np_data_two=df_data_two.values
-	#np_data_two=np_data_two.astype('float')
-	########instance to object 
-	instance_one=logistic_data()
 	########set paramters
 	iters=50000
 	alpha=0.08
@@ -203,12 +209,27 @@ if __name__=='__main__':
 	#logistic 2 分类
 	logger=logging.getLogger()
 	logger.setLevel(logging.INFO)
-	#instance_one.logistic_fit_normal(np_data_two[:,0],np_data_two[:,1],np_data_two[:,0:2],instance_one.array_oneTotwo(np_data_two[:,2]),iters,alpha)
-	#logistic 多 分类
-	#instance_one.logistic_fit_for_three(np_X_three[:,0],np_X_three[:,1],np_X_three[:,0:2],Y_Three,iters,learningrate)
-	#logistaic 非线性 多类
-	instance_one.logistic_fit_for_three_nolinear(np_X_three[:,0],np_X_three[:,1],np_X_three[:,0:2],Y_Three,iters,learningrate)
-	########
+	#选取2/3为训练集。1/3为测试集
+	train_features, test_features, train_labels, test_labels = train_test_split(
+        np_X_three[:,0:2], Y_Three, test_size=0.33, random_state=23323)
+	#print(train_features)
+	train_features = rebuild_features(train_features)
+	test_features = rebuild_features(test_features)
+	print('Start training')
+	time_2 = time.time()
+	met = max_entropy_model_data()
+	met.train(train_features, train_labels)
+
+	time_3 = time.time()
+	print('training cost ', time_3 - time_2, ' second', '\n')
+
+	print('Start predicting')
+	test_predict = met.predict(test_features)
+	time_4 = time.time()
+	print('predicting cost ', time_4 - time_3, ' second', '\n')
+
+	score = accuracy_score(met.array_twoToone(test_labels), test_predict)
+	print("The accruacy socre is ", score)
 	
 	
 
